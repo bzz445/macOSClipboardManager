a085a3fb5e717046813c7271f8b3d9e408e5378d
Morozov <morozov_pv@taximaxim.ru>
2020-06-16 11:27:04 +0500

    Initial Commit

---
+++ ClipboardManager/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClipboardManager
+//
+//  Created by Pavel Morozov on 16.06.2020.
+//  Copyright © 2020 Pavel Morozov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
